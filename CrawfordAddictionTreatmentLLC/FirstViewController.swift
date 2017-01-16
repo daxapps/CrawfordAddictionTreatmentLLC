3efc1b353fdaa05ecdd0846da440854442af9170
daxapps <daxapps777@gmail.com>
2017-01-16 11:40:20 -0600

    create MessageComposer.swift file

--- CrawfordAddictionTreatmentLLC/FirstViewController.swift
+++ CrawfordAddictionTreatmentLLC/FirstViewController.swift
@@ -9,16 +9,11 @@
 import UIKit
 import MessageUI
 
-class FirstViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+class FirstViewController: UIViewController {
 
     @IBOutlet weak var callButton: UIButton!
     @IBOutlet weak var textUsButton: UIButton!
   
-//    @IBAction func onlineAppointmentButton(_ sender: Any) {
-//        if let url = URL(string: "https://johncrawfordmd.com/online-appointments/") {
-//            UIApplication.shared.open(url)
-//        }
-//    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -37,7 +32,7 @@ class FirstViewController: UIViewController, MFMessageComposeViewControllerDeleg
     @IBAction func sendTextMessageButtonPressed(_ sender: Any) {
         
         // Create a MessageComposer
-        let messageComposer = TextUsViewController()
+        let messageComposer = MessageComposer()
         
         // Make sure the device can send text messages
         if (messageComposer.canSendText()) {
@@ -48,54 +43,15 @@ class FirstViewController: UIViewController, MFMessageComposeViewControllerDeleg
             present(messageComposeVC, animated: true, completion: nil)
         } else {
             // Let the user know if his/her device isn't able to send text messages
-//            let errorAlert = UIAlertView(title: "Cannot Send Text Message", message: "Your device is not able to send text messages.", delegate: self, cancelButtonTitle: "OK")
-//            errorAlert.show()
+            let errorAlert = UIAlertController(title: "Cannot Send Text Message", message: "Your device is not able to send text messages.", preferredStyle: .alert)
+                //UIAlertView(title: "Cannot Send Text Message", message: "Your device is not able to send text messages.", delegate: self, cancelButtonTitle: "OK")
+            errorAlert.show(errorAlert, sender: self)
         }
     }
     
-    // A wrapper function to indicate whether or not a text message can be sent from the user's device
-    func canSendText() -> Bool {
-        return MFMessageComposeViewController.canSendText()
-    }
-    
-    // Configures and returns a MFMessageComposeViewController instance
-    func configuredMessageComposeViewController() -> MFMessageComposeViewController {
-        let messageComposeVC = MFMessageComposeViewController()
-        messageComposeVC.messageComposeDelegate = self  //  Make sure to set this property to self, so that the controller can be dismissed!
-        messageComposeVC.recipients = ["3379359222"]
-        messageComposeVC.body =  ""
-        return messageComposeVC
-    }
-    
-    // MFMessageComposeViewControllerDelegate callback - dismisses the view controller when the user is finished with it
-    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
-        controller.dismiss(animated: true, completion: nil)
-    }
-
 }
 
 
-class TextMessageComposer: NSObject, MFMessageComposeViewControllerDelegate {
-    
-    // A wrapper function to indicate whether or not a text message can be sent from the user's device
-    func canSendText() -> Bool {
-        return MFMessageComposeViewController.canSendText()
-    }
-    
-    // Configures and returns a MFMessageComposeViewController instance
-    func configuredMessageComposeViewController() -> MFMessageComposeViewController {
-        let messageComposeVC = MFMessageComposeViewController()
-        messageComposeVC.messageComposeDelegate = self  //  Make sure to set this property to self, so that the controller can be dismissed!
-        messageComposeVC.recipients = ["3379359222"]
-        messageComposeVC.body =  ""
-        return messageComposeVC
-    }
-    
-    // MFMessageComposeViewControllerDelegate callback - dismisses the view controller when the user is finished with it
-    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
-        controller.dismiss(animated: true, completion: nil)
-    }
-    
-}
+
 
 
