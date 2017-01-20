cb2a414b0b9f18d0579eacf2bab3bed4779a6e7e
daxapps <daxapps777@gmail.com>
2017-01-20 11:37:48 -0600

    rename FirstVC to HomeVC

--- CrawfordAddictionTreatmentLLC/HomeViewController.swift
+++ CrawfordAddictionTreatmentLLC/HomeViewController.swift
@@ -19,6 +19,11 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
     @IBAction func makeCall(_ sender: Any) {
         let url = URL(string: "telprompt://3379359222")!
         UIApplication.shared.open(url)
