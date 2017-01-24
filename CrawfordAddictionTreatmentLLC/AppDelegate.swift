6879a21974a02ccc7fe073d3d2c1cd04c9414996
daxapps <daxapps777@gmail.com>
2017-01-23 20:29:44 -0600

    Remove Pod References in Build Settings

--- CrawfordAddictionTreatmentLLC/AppDelegate.swift
+++ CrawfordAddictionTreatmentLLC/AppDelegate.swift
@@ -8,9 +8,6 @@
 
 import UIKit
 import CoreData
-//import Firebase
-//import FirebaseAuthUI
-//import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,13 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-//        FIRApp.configure()
         return true
     }
     
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-//        return FUIAuth.defaultAuthUI()?.handleOpen(url, sourceApplication: sourceApplication ?? "Try Again Later") ?? false
         return true 
     }
 
